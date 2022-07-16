f3b80b24b231a7eba75e6eb217216dd2e6ea086e
raghav2005 <raghavawasthi2005@gmail.com>
2022-07-16 18:07:31 +0400

    added comments

--- raghav_os/src/main.rs
+++ raghav_os/src/main.rs
@@ -1,15 +1,19 @@
+// don't link Rust standard library
 #![no_std]
+// disable all Rust-level entry points
 #![no_main]
 
 use core::panic::PanicInfo;
 
-// function called when panicking
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+// don't mangle name of function
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+	// this function is the entry point, since linker looks for a function named `_start` by default
 	loop {}
 }
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+// function called on panick
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
 	loop {}
 }
