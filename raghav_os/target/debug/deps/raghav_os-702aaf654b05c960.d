a65b09d206d26b7b18fd76b76ba69ae17c35fbd0
raghav2005 <raghavawasthi2005@gmail.com>
2022-07-16 17:59:36 +0400

    added additional args to linker to work on macOS

---
+++ raghav_os/target/debug/deps/raghav_os-702aaf654b05c960.d
@@ -0,0 +1,5 @@
+/Users/raghav/OneDrive/Coding/OSDevelopment/RaghavOS-in-Rust/raghav_os/target/debug/deps/raghav_os-702aaf654b05c960: src/main.rs
+
+/Users/raghav/OneDrive/Coding/OSDevelopment/RaghavOS-in-Rust/raghav_os/target/debug/deps/raghav_os-702aaf654b05c960.d: src/main.rs
+
+src/main.rs:
---
+++ raghav_os/target/debug/raghav_os.d
@@ -0,0 +1,1 @@
+/Users/raghav/OneDrive/Coding/OSDevelopment/RaghavOS-in-Rust/raghav_os/target/debug/raghav_os: /Users/raghav/OneDrive/Coding/OSDevelopment/RaghavOS-in-Rust/raghav_os/src/main.rs
