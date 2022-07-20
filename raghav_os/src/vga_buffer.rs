845d823345316d44a178209e0aa69de6dbaf180a
raghav2005 <raghavawasthi2005@gmail.com>
2022-07-20 21:52:47 +0400

    added functionality to have new lines

--- raghav_os/src/vga_buffer.rs
+++ raghav_os/src/vga_buffer.rs
@@ -59,6 +59,33 @@ impl ColorCode {
 
 impl Writer {
 
+	fn new_line(&mut self) {
+
+		for row in 1..BUFFER_HEIGHT {
+			for col in 0..BUFFER_WIDTH {
+				let character = self.buffer.chars[row][col].read();
+				self.buffer.chars[row - 1][col].write(character);
+			}
+		}
+
+		self.clear_row(BUFFER_HEIGHT - 1);
+		self.column_position = 0;
+
+	}
+
+	fn clear_row(&mut self, row: usize) {
+
+		let blank = ScreenChar {
+			ascii_character: b' ',
+			color_code: self.color_code,
+		};
+
+		for col in 0..BUFFER_WIDTH {
+			self.buffer.chars[row][col].write(blank);
+		}
+
+	}
+
 	pub fn write_byte(&mut self, byte: u8) {
 		match byte {
 
@@ -83,8 +110,6 @@ impl Writer {
 			}
 		}
 	}
-
-	fn new_line(&mut self) {/* TODO */}
 
 	pub fn write_string(&mut self, s: &str) {
 		for byte in s.bytes() {
